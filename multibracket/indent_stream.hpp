47b61b12f35c3ca12e208eca479bb965d039ad12
Mattias <mattias.s.sjo@gmail.com>
2020-11-02 14:30:58 +0100

    Separate utility classes into separate headers, make insertion_order_map STD-compliant

---
+++ multibracket/indent_stream.hpp
@@ -0,0 +1,166 @@
+#ifndef INDENT_STREAM_H
+#define INDENT_STREAM_H
+
+#include <iostream>
+#include <sstream>
+
+
+namespace detail {
+
+    class indent_buf : public std::stringbuf {
+    private:
+        
+        std::ostream& out;
+        
+        size_t depth;
+        size_t indent_level;
+        
+        void indent_line(bool par = false){
+            depth = (par ? par_indent : basic_indent) + indent_level*indent_step;
+                    
+            if(depth > max_depth)
+                throw std::runtime_error("ERROR: indent larger than maximum depth");
+            
+            std::cout << std::string(depth, ' ');
+        }
+    public:
+        
+        size_t max_depth = 80;
+        size_t par_indent = 2;
+        size_t basic_indent = 0;
+        size_t indent_step = 4;
+        
+        indent_buf(std::ostream& o = std::cout, size_t ind = 0) : out(o), indent_level(ind) {
+            indent_line();
+        }
+        
+        virtual int sync(){
+            std::string s = str();
+            
+            for(size_t i = 0; i < s.length(); i++){
+                if(depth > max_depth || s[i] == '\n'){
+                    out.put('\n');
+                    indent_line();
+                    
+                    if(s[i] == '\n')
+                        continue;
+                }  
+                
+                out.put(s[i]);
+                depth++;
+                
+            }
+            
+            str("");        
+        }
+        
+        void paragraph(){
+            sync();
+            out.put('\n');
+            indent_line(true);
+        }
+        
+        void incr_indent(size_t incr){
+            indent_level += incr;
+        }
+        void decr_indent(size_t decr){
+            if(indent_level < decr)
+                throw std::runtime_error("ERROR: negative indent");
+                
+            indent_level -= decr;
+        }
+        void set_indent(size_t level){
+            indent_level = level;
+        }
+    };
+}
+
+/**
+ * @brief an ostream that automatically indents its output.
+ * 
+ * An indent stream wraps another ostream (default: @c std::cout) and normally 
+ * prints verbatim to that stream, but when the length of the current line
+ * exceeds a certain number of characters (default: 80), a linebreak is inserted.
+ * Whenever a new line is started, either this way or by printing a linebreak,
+ * the next line is indented. The number of indent steps can be incremented and
+ * decremented as needed, and the size of each step can be set (default: 4 spaces).
+ * Instead of normal linebreaks, the @c paragraph can be called to break the
+ * line and make a special paragraph indent, which can be set deeper or shallower
+ * than the normal indent (default: 2 extra spaces). It is also possible to set
+ * a number of spaces that always appears to the left regardless of the indent
+ * level (default: 0).
+ * 
+ * This class has no way to guard against independent use of the underlying
+ * ostream, and will not behave correctly in that case. Currently, it does not 
+ * handle tabs and non-printable characters correctly. 
+ */
+class indent_stream : public std::ostream {
+private:
+    detail::indent_buf& indent_buf(){
+        return *( static_cast<detail::indent_buf*>(rdbuf()) );
+    }
+    
+    const detail::indent_buf& indent_buf() const {
+        return *( static_cast<detail::indent_buf*>(rdbuf()) );
+    }
+        
+public:
+    indent_stream(std::ostream& out = std::cout, size_t indent = 0) 
+    : std::ostream(new detail::indent_buf(out, indent)) 
+    {}
+    
+    virtual ~indent_stream() {
+        indent_buf().sync();
+        delete &indent_buf();
+    }
+    
+    indent_stream& paragraph(){ 
+        indent_buf().paragraph();   
+        return *this; 
+    }
+    indent_stream& incr_indent(size_t incr = 1){ 
+        indent_buf().incr_indent(incr); 
+        return *this; 
+    }
+    indent_stream& decr_indent(size_t decr = 1){ 
+        indent_buf().decr_indent(decr); 
+        return *this; 
+    }
+    indent_stream& set_indent(size_t level){
+        indent_buf().set_indent(level);
+        return *this;
+    }
+    
+    size_t get_indent_step() const {        
+        return indent_buf().indent_step;                
+    }
+    indent_stream& set_indent_step(size_t new_step){  
+        indent_buf().indent_step = new_step;
+        return *this;
+    }
+    size_t get_max_depth() const {          
+        return indent_buf().max_depth;                  
+    }
+    indent_stream& set_max_depth(size_t new_depth){   
+        indent_buf().max_depth = new_depth; 
+        return *this;
+    }
+    size_t get_basic_indent() const {       
+        return indent_buf().basic_indent;               
+    }
+    indent_stream& set_basic_indent(size_t new_ind){  
+        indent_buf().basic_indent = new_ind;
+        return *this;
+    }
+    size_t get_par_indent() const {         
+        return indent_buf().par_indent;                 
+    }
+    indent_stream& set_par_indent(int extra_ind){     
+        if(-extra_ind > indent_buf().basic_indent)
+            throw std::runtime_error("ERROR: paragraph indent would be negative");
+        indent_buf().par_indent = indent_buf().basic_indent + extra_ind;
+        return *this;
+    }
+};
+
+#endif
---
+++ multibracket/insertion_order_map.hpp
@@ -0,0 +1,230 @@
+#ifndef INSERTION_ORDER_MAP_H
+#define INSERTION_ORDER_MAP_H
+
+#include <unordered_map>
+#include <list>
+
+/**
+ * @brief Map type where elements are ordered not by some comparison
+ * operator, but by the chronological order in which they are inserted.
+ * 
+ * @tparam KeyT the type of the keys
+ * @tparam MappedT the type of the mapped values
+ * @tparam Hash the has object used to hash the keys; defaults to @c std::hash<KeyT>.
+ * @tparam Allocator the allocator used to allocate values; 
+ *      defaults to @c std::allocator<value_type> where @c value_type is a key-value pair.
+ * 
+ * For most purposes, this map behaves exactly like an @c std::unordered_map with the same
+ * template parameters. The complexity of all operations is the same, albeit with a slight
+ * time and space overhead. The main difference is that iterators to the map traverse it
+ * in the order elements were added, as if the map were a @c std::list into which @c insert 'ed
+ * elements are @c push_back 'ed. Existing elements can be modified without modifying their
+ * place in the order, but erased elements can not be reinserted in their old locations.
+ */
+template< 
+    typename KeyT, 
+    typename MappedT, 
+    class Hash = std::hash<KeyT>,
+    class Allocator = std::allocator< std::pair<const KeyT, MappedT> >
+>
+class insertion_order_map {
+public:
+    using key_type                  = KeyT;
+    using mapped_type               = MappedT;
+    using value_type                = std::pair<const key_type, mapped_type>;
+    using size_type                 = std::size_t;
+    using allocator_type            = Allocator;
+    using reference                 = mapped_type&;
+    using const_reference           = const mapped_type&;
+    using pointer                   = typename std::allocator_traits<allocator_type>::pointer;
+    using const_pointer             = typename std::allocator_traits<allocator_type>::const_pointer;
+    
+private:
+    using key_val_list = typename std::list< value_type, Allocator >;
+    
+public:
+    using iterator                  = typename key_val_list::iterator;
+    using const_iterator            = typename key_val_list::const_iterator;
+    using reverse_iterator          = typename key_val_list::reverse_iterator;
+    using const_reverse_iterator    = typename key_val_list::const_reverse_iterator;
+    
+private:
+    using key_iter_map = typename std::unordered_map< key_type, iterator >;
+    
+private:
+    key_val_list elements;
+    key_iter_map map;
+    
+public:
+    /* Constructors, destructors and assignment */
+    insertion_order_map(const insertion_order_map&) = default;
+    insertion_order_map(insertion_order_map&&) = default;
+    
+    ~insertion_order_map() = default;
+    
+    explicit insertion_order_map(const allocator_type& a = allocator_type()) 
+    : elements(a), map() {};
+    
+    insertion_order_map(std::initializer_list<value_type> il, const allocator_type& a = allocator_type()) 
+    : elements(il, a), map(){
+        for(iterator it = elements.begin(); it != elements.end(); ++it)
+            map[ it->first ] = it;
+    }
+    template <typename FwdIter>
+    insertion_order_map(FwdIter first, FwdIter last, const allocator_type& a = allocator_type()) 
+    : elements(first, last, a), map(){
+        for(iterator it = elements.begin(); it != elements.end(); ++it)
+            map[ it->first ] = it;
+    }
+
+    insertion_order_map& operator= (const insertion_order_map&) = default;
+    insertion_order_map& operator= (insertion_order_map&&) = default;
+    
+    /* Selectors */
+    const_iterator find(const key_type& key) const {
+        auto where = map.find(key);
+        if(where != map.cend())
+            return where->second;
+        else
+            return cend();
+    }
+    size_type size() const {
+        return elements.size();         //equivalently, map.size()
+    }
+    size_type max_size() const {
+        return std::min( elements.max_size(), map.max_size() );
+    }
+    bool empty() const {
+        return elements.empty();        //equivalently, map.empty()
+    }
+    const_reference at(const key_type& key) const {
+        return *( map.at(key).second );
+    }
+    
+    allocator_type get_allocator()  {
+        return elements.get_allocator();
+    }
+
+    /* Mutators */
+    iterator find(const key_type& key){
+        auto where = map.find(key);
+        if(where != map.end())
+            return where->second;
+        else
+            return end();
+    }
+    reference at(const key_type& key){
+        return *( map.at(key).second );
+    }
+    
+    std::pair<iterator, bool> insert(const value_type& value){
+        auto where = map.find(value.first);
+        if(where != map.end()){
+            return std::make_pair(where, false);
+        }
+        else{
+            //create new element in back
+            elements.push_back( value );
+            return (map[value.first] = --elements.end())->second;
+        }         
+    }
+    std::pair<iterator, bool> insert(value_type&& value){
+        auto where = map.find(value.first);
+        if(where != map.end()){
+            return std::make_pair(where, false);
+        }
+        else{
+            //create new element in back
+            elements.push_back( value );
+            return (map[value.first] = --elements.end())->second;
+        }         
+    }
+    template< typename InputIt >
+    void insert(InputIt first, InputIt last){
+        for(; first != last; ++first){
+            auto where = map.find( first->first );
+            if(where == map.end()){
+                elements.push_back( *first );
+                map[first->first] = --elements.end();
+            }
+        }
+    }
+    void insert(std::initializer_list<value_type> ilist){
+        for(value_type& value : ilist){
+            auto where = map.find( value.first );
+            if(where == map.end()){
+                elements.push_back( value );
+                map[value.first] = --elements.end();
+            }
+        }
+    }
+    template< class... Args >
+    std::pair<iterator, bool> emplace( Args&&... args ){
+        auto& it_bool = elements.emplace(args...);
+        if(it_bool.second)
+            map[it_bool.first->first] = it_bool.first;
+        
+        return it_bool;
+    }
+    
+    //Hinted insertion is pointless, but included for compatibility
+    iterator insert(const_iterator hint, const value_type& value){  return insert(value).first; }
+    iterator insert(const_iterator hint, value_type&& value){       return insert(value).first; }
+    template <class... Args>
+    iterator emplace_hint( const_iterator hint, Args&&... args ){   return emplace(args...).first;}
+    
+    reference operator[] (const key_type& key){
+        auto where = map.find(key);
+        if(where != map.end()){
+            //where = {key, iterator to {key, val}} so the expression below is val
+            return where->second->second;
+        }
+        else{
+            //create new element in back
+            elements.push_back( value_type(key, mapped_type()) );
+            map[key] = --elements.end();
+            return elements.back().second;
+        }           
+    }
+    
+    size_type erase(const key_type& key){
+        auto where = map.find(key);
+        if(where != map.end()){
+            elements.erase(where->second);
+            map.erase(where);
+            return 1;
+        }
+        else
+            return 0;        
+    }
+    iterator erase(iterator pos){
+        map.erase(pos->first);
+        return elements.erase(pos);
+    }
+    iterator erase(iterator first, iterator last){
+        for(; first != last; first = elements.erase(first))
+            map.erase(first->first);
+        return first;
+    }
+    
+    void clear(){
+        elements.clear();
+        map.clear();
+    }
+    
+    iterator begin()                { return elements.begin();      }
+    iterator end()                  { return elements.end();        }
+    const_iterator begin()  const   { return elements.begin();      }
+    const_iterator end()    const   { return elements.end();        }
+    const_iterator cbegin() const   { return elements.cbegin();     }
+    const_iterator cend()   const   { return elements.cend();       }
+    reverse_iterator rbegin()                { return elements.rbegin();      }
+    reverse_iterator rend()                  { return elements.rend();        }
+    const_reverse_iterator rbegin()  const   { return elements.rbegin();      }
+    const_reverse_iterator rend()    const   { return elements.rend();        }
+    const_reverse_iterator crbegin() const   { return elements.crbegin();     }
+    const_reverse_iterator crend()   const   { return elements.crend();       }
+    
+};
+
+#endif
